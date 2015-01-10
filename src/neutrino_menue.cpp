e9f952073045668ee0eaa891cff1321ea004228b
Frankenstone <dampf_acc@yahoo.com>
2015-01-10 11:47:05 +0100

    fix last commit

--- src/neutrino_menue.cpp
+++ src/neutrino_menue.cpp
@@ -252,7 +252,7 @@ void CNeutrinoApp::InitMenuMain()
 	//4th section***************************************************************************************************
 
 	//infomenu
-#if BOXMODEL_UFS910
+#ifdef BOXMODEL_UFS910
 	mf = new CMenuForwarder(LOCALE_MESSAGEBOX_INFO, true, NULL, new CInfoMenu(), NULL, CRCInput::RC_help);
 	mf->setHint(NEUTRINO_ICON_HINT_INFO, LOCALE_MENU_HINT_INFO);
 	personalize.addItem(MENU_MAIN, mf, &g_settings.personalize[SNeutrinoSettings::P_MAIN_INFOMENU]);
@@ -461,7 +461,7 @@ void CNeutrinoApp::InitMenuService()
 	//2nd section***************************************************************************************************
 
 	//infomenu
-#if BOXMODEL_UFS910
+#ifdef BOXMODEL_UFS910
 	mf = new CMenuForwarder(LOCALE_MESSAGEBOX_INFO, true, NULL, new CInfoMenu(), NULL, CRCInput::RC_help);
 	mf->setHint(NEUTRINO_ICON_HINT_INFO, LOCALE_MENU_HINT_INFO);
 	personalize.addItem(MENU_SERVICE, mf, &g_settings.personalize[SNeutrinoSettings::P_MSER_SERVICE_INFOMENU]);

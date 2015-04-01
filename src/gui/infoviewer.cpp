08f692f845d6d50835d223ca8d6596a9c2b68f85
Frankenstone <dampf_acc@yahoo.com>
2015-04-01 08:12:27 +0200

    fix last pick

--- src/gui/infoviewer.cpp
+++ src/gui/infoviewer.cpp
@@ -1677,11 +1677,10 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 		//printf("paintProgressBar(%d, %d, %d, %d)\n", BoxEndX - pb_w - SHADOW_OFFSET, ChanNameY - (pb_h + 10) , pb_w, pb_h);
 	}
 
-	if (!g_settings.gradiant)
 	if (showButtonBar) {
 		if (!g_settings.gradiant)
 			frameBuffer->paintHLine(ChanInfoX + 10, BoxEndX - 10, CurrInfoY - height - 2, COL_INFOBAR_PLUS_3);
-		frameBuffer->paintHLine(ChanInfoX + 10, BoxEndX - 10, NextInfoY + 2, COL_INFOBAR_PLUS_3);
+			frameBuffer->paintHLine(ChanInfoX + 10, BoxEndX - 10, NextInfoY + 2, COL_INFOBAR_PLUS_3);
 	}
 
 

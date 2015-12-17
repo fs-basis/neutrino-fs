9d634a4da7b7e88d5cff947d5285810a3f1c6375
Frankenstone <dampf_acc@yahoo.com>
2015-12-17 18:44:07 +0100

    fix merge #1

--- src/gui/lua/lua_curl.h
+++ src/gui/lua/lua_curl.h
@@ -16,6 +16,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
+#include <curl/curl.h>
+#include <curl/easy.h>
 
 #ifndef _LUACURL_H
 #define _LUACURL_H

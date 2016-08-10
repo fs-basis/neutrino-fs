c99b0f8866dabc2b0f7c5f75a019bc2dff4b12b4
Frankenstone <dampf_acc@yahoo.com>
2016-08-10 20:12:55 +0200

    small fix

--- src/gui/moviebrowser.cpp
+++ src/gui/moviebrowser.cpp
@@ -1391,8 +1391,7 @@ void CMovieBrowser::info_hdd_level(bool /* paint_hdd */)
 	if (tmp_blocks_percent_used != blocks_percent_used || paint_hdd) {
 		tmp_blocks_percent_used = blocks_percent_used;
 */
-	if (true) {
-	//if (g_settings.infobar_show_sysfs_hdd) {
+	if (g_settings.infobar_show_sysfs_hdd) {
 		const short pbw = 100;
 		const short border = m_cBoxFrameTitleRel.iHeight/4;
 		CProgressBar pb(m_cBoxFrame.iX+ m_cBoxFrameFootRel.iWidth - pbw - border, m_cBoxFrame.iY+m_cBoxFrameTitleRel.iY + border, pbw, m_cBoxFrameTitleRel.iHeight/2);

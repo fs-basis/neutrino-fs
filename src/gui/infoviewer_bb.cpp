12f622a75dbd185152a32ba39db3d4142e762985
Frankenstone <dampf_acc@yahoo.com>
2017-01-30 19:35:19 +0100

    small fix src/gui/infoviewer_bb.cpp

--- src/gui/infoviewer_bb.cpp
+++ src/gui/infoviewer_bb.cpp
@@ -63,6 +63,7 @@
 
 extern CRemoteControl *g_RemoteControl;	/* neutrino.cpp */
 extern cVideo * videoDecoder;
+extern CPictureViewer * g_PicViewer;
 
 #define COL_INFOBAR_BUTTONS_BACKGROUND (COL_MENUFOOT_PLUS_0)
 

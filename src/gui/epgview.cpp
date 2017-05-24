d4fc066669e9dd161a4c095e422e952374db2882
Frankenstone <dampf_acc@yahoo.com>
2017-05-24 11:53:50 +0200

    Revert "Revert "CEpgData: supplement to 'fix header size'""
    This reverts commit c6d283d2f1aa3090e77daeb5df5052511ef75163.

--- src/gui/epgview.cpp
+++ src/gui/epgview.cpp
@@ -128,7 +128,7 @@ CEpgData::CEpgData()
 	header     	= NULL;
 	Bottombox 	= NULL;
 	pb		= NULL;
-	font_title	= g_Font[SNeutrinoSettings::FONT_TYPE_EPG_TITLE];
+	font_title	= NULL;
 }
 
 CEpgData::~CEpgData()
@@ -141,6 +141,7 @@ void CEpgData::start()
 	ox = frameBuffer->getScreenWidthRel(bigFonts ? false /* big */ : true /* small */);
 	oy = frameBuffer->getScreenHeightRel(bigFonts ? false /* big */ : true /* small */);
 
+	font_title   = g_Font[SNeutrinoSettings::FONT_TYPE_EPG_TITLE];
 	topheight    = font_title->getHeight();
 	topboxheight = topheight + 6;
 	botboxheight = g_Font[SNeutrinoSettings::FONT_TYPE_EPG_DATE]->getHeight() + 6;
@@ -845,7 +846,6 @@ int CEpgData::show(const t_channel_id channel_id, uint64_t a_id, time_t* a_start
 	// header + logo
 	if (!header){
 		header = new CComponentsHeader(sx, sy, ox, toph);
-		header->setCaptionFont(font_title);
 		header->setColorBody(COL_MENUHEAD_PLUS_0);
 		header->enableColBodyGradient(g_settings.theme.menu_Head_gradient, COL_MENUCONTENT_PLUS_0, g_settings.theme.menu_Head_gradient_direction);
 		header->enableClock(true, "%H:%M", "%H %M", true);
@@ -855,6 +855,7 @@ int CEpgData::show(const t_channel_id channel_id, uint64_t a_id, time_t* a_start
 		header->getClockObject()->setBlit(false);
 	}
 
+	header->setCaptionFont(font_title);
 	header->setCaption(epgData.title);
 
 	if (header->isPainted())

9e4176d3ce63e908e950f5fee4a209ad8f696d38
Frankenstone <dampf_acc@yahoo.com>
2017-11-30 17:00:08 +0100

    fix unused files
    - webradio_setup.cpp
    - webradio_setup.h

--- src/gui/mediaplayer_setup.cpp
+++ src/gui/mediaplayer_setup.cpp
@@ -45,7 +45,6 @@
 #include <gui/audioplayer_setup.h>
 #include <gui/pictureviewer_setup.h>
 #include <gui/webtv_setup.h>
-#include <gui/webradio_setup.h>
 #include <gui/moviebrowser/mb.h>
 
 #include <driver/screen_max.h>

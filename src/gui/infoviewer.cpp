389c128efe3512989ffc8cadf3f0faa1e5d5727e
Frankenstone <dampf_acc@yahoo.com>
2018-01-16 17:10:12 +0100

    Revert "fix merge"
    This reverts commit b17cbb19e160570aff78458057a341fb07e2bef6.

--- src/gui/infoviewer.cpp
+++ src/gui/infoviewer.cpp
@@ -1558,7 +1558,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
             txt_curr_rest = NULL;
         }
 
-        txt_curr_event = new CComponentsTextTransp(NULL, xStart, CurrInfoY - height, currTimeX - xStart, height, current,
+        txt_curr_event = new CComponentsTextTransp(NULL, xStart, CurrInfoY - height, currTimeX - xStart - OFFSET_INNER_SMALL, height, current,
                 CTextBox::NO_AUTO_LINEBREAK, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO],
                 CComponentsText::FONT_STYLE_REGULAR, colored_event_C ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
         txt_curr_event->paint(CC_SAVE_SCREEN_YES);
@@ -1603,7 +1603,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
             txt_next_in = NULL;
         }
 
-        txt_next_event = new CComponentsTextTransp(NULL, xStart, NextInfoY, nextTimeX - xStart, height, next,
+        txt_next_event = new CComponentsTextTransp(NULL, xStart, NextInfoY, nextTimeX - xStart - OFFSET_INNER_SMALL, height, next,
                 CTextBox::NO_AUTO_LINEBREAK, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO],
                 CComponentsText::FONT_STYLE_REGULAR, colored_event_N ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
         txt_next_event->paint(CC_SAVE_SCREEN_YES);

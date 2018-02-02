d923e7e68f2bdcb0003b43b8ccc66395954dcceb
Frankenstone <dampf_acc@yahoo.com>
2018-02-02 12:19:14 +0100

    Revert "fontrenderer: allow to render into a memory buffer"
    This reverts commit 042eed030af4dc77e879f9110a1e8dfa85dca7fe.

--- src/driver/fontrenderer.h
+++ src/driver/fontrenderer.h
@@ -4,7 +4,6 @@
 	Copyright (C) 2001 Steffen Hehn 'McClean'
         Copyright (C) 2003 thegoodguy
         Copyright (C) 2013-2017 M. Liebmann (micha-bbg)
-	Copyright (C) 2009,2017-2018 Stefan Seyfried
 
 	License: GPL
 
@@ -76,8 +75,8 @@ class Font
 			FULLBG = 2
 		};
 
-	void RenderString(int x, int y, const int width, const char *        text, const fb_pixel_t color, const int boxheight = 0, const unsigned int flags = IS_UTF8, fb_pixel_t *buffer = NULL, int stride = 0);
-	void RenderString(int x, int y, const int width, const std::string & text, const fb_pixel_t color, const int boxheight = 0, const unsigned int flags = IS_UTF8, fb_pixel_t *buffer = NULL, int stride = 0);
+	void RenderString(int x, int y, const int width, const char *        text, const fb_pixel_t color, const int boxheight = 0, const unsigned int flags = IS_UTF8);
+	void RenderString(int x, int y, const int width, const std::string & text, const fb_pixel_t color, const int boxheight = 0, const unsigned int flags = IS_UTF8);
 
 	int getRenderWidth(const char *        text, const bool utf8_encoded = true);
 	int getRenderWidth(const std::string & text, const bool utf8_encoded = true);

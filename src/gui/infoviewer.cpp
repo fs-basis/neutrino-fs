neutrino-mp/neutrino-mp

infoviewer: force repaint of text fields
next try to get rid of font artifacts:
* use kill() instead of hide() to force full repaint at next paint()
  (this is probably not the best way, but works for now)

src/gui/infoviewer.cpp
@@ -1745,7 +1745,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 			txt_cur_event = new CComponentsTextTransp(NULL, xStart, CurrInfoY - height, currTimeX - xStart, height);
 		else {
 			if (txt_cur_event->isPainted())
-				txt_cur_event->hide();
+				txt_cur_event->kill();
 			txt_cur_event->setDimensionsAll(xStart, CurrInfoY - height, currTimeX - xStart, height);
 		}
 		txt_cur_event->setText(current, CTextBox::NO_AUTO_LINEBREAK, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO], colored_event_C ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
@@ -1756,7 +1756,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 				txt_cur_start = new CComponentsTextTransp(NULL, InfoX, CurrInfoY - height, info_time_width, height);
 			else {
 				if (txt_cur_start->isPainted())
-					txt_cur_start->hide();
+					txt_cur_start->kill();
 				txt_cur_start->setDimensionsAll(InfoX, CurrInfoY - height, info_time_width, height);
 			}
 			txt_cur_start->setText(runningStart, CTextBox::NO_AUTO_LINEBREAK, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO], colored_event_C ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
@@ -1768,7 +1768,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 				txt_cur_event_rest = new CComponentsTextTransp(NULL, currTimeX, CurrInfoY - height, currTimeW, height);
 			else {
 				if (txt_cur_event_rest->isPainted())
-					txt_cur_event_rest->hide();
+					txt_cur_event_rest->kill();
 				txt_cur_event_rest->setDimensionsAll(currTimeX, CurrInfoY - height, currTimeW, height);
 			}
 			txt_cur_event_rest->setText(runningRest, CTextBox::RIGHT, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO], colored_event_C ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
@@ -1783,7 +1783,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 			txt_next_event = new CComponentsTextTransp(NULL, xStart, NextInfoY, nextTimeX - xStart, height);
 		else {
 			if (txt_next_event->isPainted())
-				txt_next_event->hide();
+				txt_next_event->kill();
 			txt_next_event->setDimensionsAll(xStart, NextInfoY, nextTimeX - xStart, height);
 		}
 		txt_next_event->setText(next, CTextBox::NO_AUTO_LINEBREAK, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO], colored_event_N ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
@@ -1794,7 +1794,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 				txt_next_start = new CComponentsTextTransp(NULL, InfoX, NextInfoY, info_time_width, height);
 			else {
 				if (txt_next_start->isPainted())
-					txt_next_start->hide();
+					txt_next_start->kill();
 				txt_next_start->setDimensionsAll(InfoX, NextInfoY, info_time_width, height);
 			}
 			txt_next_start->setText(nextStart, CTextBox::NO_AUTO_LINEBREAK, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO], colored_event_N ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
@@ -1806,7 +1806,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 				txt_next_in = new CComponentsTextTransp(NULL, nextTimeX, NextInfoY, nextTimeW, height);
 			else {
 				if (txt_next_in->isPainted())
-					txt_next_in->hide();
+					txt_next_in->kill();
 				txt_next_in->setDimensionsAll(nextTimeX, NextInfoY, nextTimeW, height);
 			}
 			txt_next_in->setText(nextDuration, CTextBox::RIGHT, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO], colored_event_N ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);

fs-basis/neutrino-fs

src/gui/infoviewer.cpp
@@ -1637,7 +1637,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 			txt_curr_event = new CComponentsTextTransp(NULL, xStart, CurrInfoY - height, currTimeX - xStart, height);
 		else {
 			if (txt_curr_event->isPainted())
-				txt_curr_event->hide();
+				txt_curr_event->kill();
 			txt_curr_event->setDimensionsAll(xStart, CurrInfoY - height, currTimeX - xStart, height);
 		}
 		txt_curr_event->setText(current, CTextBox::NO_AUTO_LINEBREAK, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO], colored_event_C ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
@@ -1648,7 +1648,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 				txt_curr_start = new CComponentsTextTransp(NULL, InfoX, CurrInfoY - height, info_time_width, height);
 			else {
 				if (txt_curr_start->isPainted())
-					txt_curr_start->hide();
+					txt_curr_start->kill();
 				txt_curr_start->setDimensionsAll(InfoX, CurrInfoY - height, info_time_width, height);
 			}
 			txt_curr_start->setText(runningStart, CTextBox::NO_AUTO_LINEBREAK, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO], colored_event_C ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
@@ -1660,7 +1660,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 				txt_curr_rest = new CComponentsTextTransp(NULL, currTimeX, CurrInfoY - height, currTimeW, height);
 			else {
 				if (txt_curr_rest->isPainted())
-					txt_curr_rest->hide();
+					txt_curr_rest->kill();
 				txt_curr_rest->setDimensionsAll(currTimeX, CurrInfoY - height, currTimeW, height);
 			}
 			txt_curr_rest->setText(runningRest, CTextBox::RIGHT, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO], colored_event_C ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
@@ -1675,7 +1675,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 			txt_next_event = new CComponentsTextTransp(NULL, xStart, NextInfoY, nextTimeX - xStart, height);
 		else {
 			if (txt_next_event->isPainted())
-				txt_next_event->hide();
+				txt_next_event->kill();
 			txt_next_event->setDimensionsAll(xStart, NextInfoY, nextTimeX - xStart, height);
 		}
 		txt_next_event->setText(next, CTextBox::NO_AUTO_LINEBREAK, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO], colored_event_N ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
@@ -1686,7 +1686,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 				txt_next_start = new CComponentsTextTransp(NULL, InfoX, NextInfoY, info_time_width, height);
 			else {
 				if (txt_next_start->isPainted())
-					txt_next_start->hide();
+					txt_next_start->kill();
 				txt_next_start->setDimensionsAll(InfoX, NextInfoY, info_time_width, height);
 			}
 			txt_next_start->setText(nextStart, CTextBox::NO_AUTO_LINEBREAK, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO], colored_event_N ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
@@ -1698,7 +1698,7 @@ void CInfoViewer::display_Info(const char *current, const char *next,
 				txt_next_in = new CComponentsTextTransp(NULL, nextTimeX, NextInfoY, nextTimeW, height);
 			else {
 				if (txt_next_in->isPainted())
-					txt_next_in->hide();
+					txt_next_in->kill();
 				txt_next_in->setDimensionsAll(nextTimeX, NextInfoY, nextTimeW, height);
 			}
 			txt_next_in->setText(nextDuration, CTextBox::RIGHT, g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO], colored_event_N ? COL_COLORED_EVENTS_TEXT : COL_INFOBAR_TEXT);
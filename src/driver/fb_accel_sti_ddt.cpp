bdf56c8f0f37ffe803f954a9a0245483bb2fead0
Frankenstone <dampf_acc@yahoo.com>
2018-02-28 19:20:12 +0100

    Revert "fb_accel_sti_ddt.cpp: disable waitForIdle"
    This reverts commit f3c7b332878280edff516fdf3aff7188cafa6aa0.

--- src/driver/fb_accel_sti_ddt.cpp
+++ src/driver/fb_accel_sti_ddt.cpp
@@ -59,10 +59,8 @@ static int backbuf_sz = 0;
 
 void CFbAccel::waitForIdle(void)
 {
-#if 0
 	OpenThreads::ScopedLock<OpenThreads::Mutex> m_lock(mutex);
 	ioctl(fb->fd, STMFBIO_SYNC_BLITTER);
-#endif
 }
 
 CFbAccel::CFbAccel(CFrameBuffer *_fb)

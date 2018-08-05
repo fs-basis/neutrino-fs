177586e0383915bc27ce5e7474a68d889465c852
Frankenstone <dampf_acc@yahoo.com>
2018-08-05 10:32:41 +0200

    fix playpause key movieplayer fix repeatblocker

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -5138,7 +5138,7 @@ void CNeutrinoApp::loadKeys(const char * fname)
 
 	/* options */
 	g_settings.menu_left_exit = tconfig.getInt32( "menu_left_exit", 1 );
-	g_settings.repeat_blocker = tconfig.getInt32("repeat_blocker", 250);
+	g_settings.repeat_blocker = tconfig.getInt32("repeat_blocker", 450);
 	g_settings.repeat_genericblocker = tconfig.getInt32("repeat_genericblocker", 100);
 	g_settings.longkeypress_duration = tconfig.getInt32("longkeypress_duration", LONGKEYPRESS_OFF);
 #if HAVE_SH4_HARDWARE

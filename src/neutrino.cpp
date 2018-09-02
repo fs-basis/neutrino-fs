450b513e1e4d5ddc3f54ffd7bb0023569514a265
Frankenstone <dampf_acc@yahoo.com>
2018-09-02 17:03:53 +0200

    neutrino.cpp: longkeypress_duration enabled by default

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -5106,7 +5106,7 @@ void CNeutrinoApp::loadKeys(const char * fname)
 	g_settings.menu_left_exit = tconfig.getInt32( "menu_left_exit", 1 );
 	g_settings.repeat_blocker = tconfig.getInt32("repeat_blocker", 450);
 	g_settings.repeat_genericblocker = tconfig.getInt32("repeat_genericblocker", 100);
-	g_settings.longkeypress_duration = tconfig.getInt32("longkeypress_duration", LONGKEYPRESS_OFF);
+	g_settings.longkeypress_duration = tconfig.getInt32("longkeypress_duration", 500); //LONGKEYPRESS_OFF);
 
 	g_settings.bouquetlist_mode = tconfig.getInt32( "bouquetlist_mode", 0 );
 	g_settings.sms_channel = tconfig.getInt32( "sms_channel", 0 );

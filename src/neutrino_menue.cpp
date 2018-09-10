33e9aa9556e6c1e641ec1049e9b921e792425ab2
Frankenstone <dampf_acc@yahoo.com>
2018-09-10 20:40:28 +0200

    readd plugins in main menu

--- src/neutrino_menue.cpp
+++ src/neutrino_menue.cpp
@@ -170,7 +170,40 @@ void CNeutrinoApp::InitMenuMain()
 	media->setHint(NEUTRINO_ICON_HINT_MEDIA, LOCALE_MENU_HINT_MEDIA);
 	personalize.addItem(MENU_MAIN, media, &g_settings.personalize[SNeutrinoSettings::P_MAIN_MEDIA]);
 
+	//seperator
+	personalize.addSeparator(MENU_MAIN);
+
 	CMenuForwarder * mf;
+
+	//games
+	bool show_games = g_Plugins->hasPlugin(CPlugins::P_TYPE_GAME);
+	mf = new CMenuForwarder(LOCALE_MAINMENU_GAMES, show_games, NULL, new CPluginList(LOCALE_MAINMENU_GAMES,CPlugins::P_TYPE_GAME));
+	mf->setHint(NEUTRINO_ICON_HINT_GAMES, LOCALE_MENU_HINT_GAMES);
+	personalize.addItem(MENU_MAIN, mf, &g_settings.personalize[SNeutrinoSettings::P_MAIN_GAMES]);
+#if 0
+	//tools
+	bool show_tools = g_Plugins->hasPlugin(CPlugins::P_TYPE_TOOL);
+	mf = new CMenuForwarder(LOCALE_MAINMENU_TOOLS, show_tools, NULL, new CPluginList(LOCALE_MAINMENU_TOOLS,CPlugins::P_TYPE_TOOL));
+	mf->setHint(NEUTRINO_ICON_HINT_SCRIPTS, LOCALE_MENU_HINT_TOOLS);
+	personalize.addItem(MENU_MAIN, mf, &g_settings.personalize[SNeutrinoSettings::P_MAIN_TOOLS]);
+
+	//scripts
+	bool show_scripts = g_Plugins->hasPlugin(CPlugins::P_TYPE_SCRIPT);
+	mf = new CMenuForwarder(LOCALE_MAINMENU_SCRIPTS, show_scripts, NULL, new CPluginList(LOCALE_MAINMENU_SCRIPTS,CPlugins::P_TYPE_SCRIPT));
+	mf->setHint(NEUTRINO_ICON_HINT_SCRIPTS, LOCALE_MENU_HINT_SCRIPTS);
+	personalize.addItem(MENU_MAIN, mf, &g_settings.personalize[SNeutrinoSettings::P_MAIN_SCRIPTS]);
+
+	//lua
+	bool show_lua = g_Plugins->hasPlugin(CPlugins::P_TYPE_LUA);
+	mf = new CMenuForwarder(LOCALE_MAINMENU_LUA, show_lua, NULL, new CPluginList(LOCALE_MAINMENU_LUA,CPlugins::P_TYPE_LUA));
+	mf->setHint(NEUTRINO_ICON_HINT_SCRIPTS, LOCALE_MENU_HINT_LUA);
+	personalize.addItem(MENU_MAIN, mf, &g_settings.personalize[SNeutrinoSettings::P_MAIN_LUA]);
+#else
+	bool show_plugins = g_Plugins->hasPlugin(CPlugins::P_TYPE_NO_GAME);
+	mf = new CMenuForwarder(LOCALE_PERSONALIZE_PLUGINS, show_plugins, NULL, new CPluginList(LOCALE_PERSONALIZE_PLUGINS, CPlugins::P_TYPE_NO_GAME));
+	mf->setHint(NEUTRINO_ICON_HINT_SCRIPTS, LOCALE_MENU_HINT_LUA);
+	personalize.addItem(MENU_MAIN, mf, &g_settings.personalize[SNeutrinoSettings::P_MAIN_LUA]);
+#endif
 
 	//separator
 	personalize.addSeparator(MENU_MAIN);

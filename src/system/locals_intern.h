56f8d938a24da25fc7037eefdce693c3dd1ac02a
vanhofen <vanhofen@gmx.de>
2018-12-16 01:17:00 +0100

    - timerlist: allow to enable/disable remote boxes

--- src/system/locals_intern.h
+++ src/system/locals_intern.h
@@ -2211,6 +2211,7 @@ const char * locale_real_names[] =
 	"remotebox_del",
 	"remotebox_head",
 	"remotebox_mod",
+	"remotebox_onoff",
 	"remotebox_pass",
 	"remotebox_port",
 	"remotebox_rbaddr",
--- src/system/settings.h
+++ src/system/settings.h
@@ -179,6 +179,7 @@ struct timer_remotebox_item
 		std::string pass;
 		std::string rbname;
 		std::string rbaddress;
+		bool enabled;
 		bool online;
 };
 

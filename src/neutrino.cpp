18113dbe0a90b70e4a77e587681e2f1111317c3f
vanhofen <vanhofen@gmx.de>
2018-12-21 22:30:34 +0100

    - move compatibility headers to /lib/hardware

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -115,10 +115,10 @@
 #endif
 #include <system/set_threadname.h>
 
-#include <audio.h>
+#include <hardware/audio.h>
 #include <ca_cs.h>
 #include <cs_api.h>
-#include <video.h>
+#include <hardware/video.h>
 #include <pwrmngr.h>
 
 #include <system/debug.h>

ac07a06a58a7e1b73248ad75fa7eff26d73de8a4
Thilo Graf <dbt@novatux.de>
2019-03-12 10:48:33 +0100

    /widget/menue.h/cpp: add missing signal OnAfterChangeOption()

--- src/gui/widget/menue.h
+++ src/gui/widget/menue.h
@@ -518,6 +518,7 @@ class CMenuOptionStringChooser : public CMenuItem
 			*optionValuePtr = val;
 		}
 		std::string getOptionValue() { return *optionValuePtr; }
+		sigc::signal<void> OnAfterChangeOption;
 };
 
 class CMenuGlobal

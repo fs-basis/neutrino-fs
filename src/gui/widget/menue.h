2dc510d11508175008762cad4a9d8865ebe6af35
Thilo Graf <dbt@novatux.de>
2019-03-18 15:53:03 +0100

    CMenuOptionStringChooser: remember last item
    useful for some cases if needed, default off

--- src/gui/widget/menue.h
+++ src/gui/widget/menue.h
@@ -493,7 +493,7 @@ class CMenuOptionStringChooser : public CMenuItem
 				std::string* pOptionValue,
 				CChangeObserver * const Observ,
 				bool Pulldown );
-
+		bool 		hold_last_item;
 	public:
 		CMenuOptionStringChooser(const neutrino_locale_t Name, std::string* OptionValue, bool Active = false,
 					 CChangeObserver* Observ = NULL, const neutrino_msg_t DirectKey = CRCInput::RC_nokey,
@@ -519,6 +519,7 @@ class CMenuOptionStringChooser : public CMenuItem
 		}
 		std::string getOptionValue() { return *optionValuePtr; }
 		sigc::signal<void> OnAfterChangeOption;
+		void rememberLastItem(bool remember = true) {hold_last_item = remember;}
 };
 
 class CMenuGlobal

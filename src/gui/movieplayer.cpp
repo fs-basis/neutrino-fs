9904eed70c41870747d114a91e00e86fc5cc89e3
Jacek Jendrzej <overx300@gmail.com>
2019-04-02 15:21:21 +0200

    movieplayer.cpp dont use enableOsdElements from lua-plugin

--- src/gui/movieplayer.cpp
+++ src/gui/movieplayer.cpp
@@ -1472,8 +1472,8 @@ bool CMoviePlayerGui::PlayFileStart(void)
 	getCurrentAudioName(is_file_player, currentaudioname);
 	if (is_file_player)
 		selectAutoLang();
-
-	enableOsdElements(MUTE);
+	if(!isLuaPlay)
+		enableOsdElements(MUTE);
 	return res;
 }
 

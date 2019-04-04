92993ae84b18a7ccafce805325dde7ad063c0d36
Frankenstone <dampf_acc@yahoo.com>
2019-04-04 17:51:06 +0200

    Revert "movieplayer.cpp dont use enableOsdElements from lua-plugin"
    This reverts commit e55730055b87e943773fdbeff6a484bb8390289f.

--- src/gui/movieplayer.cpp
+++ src/gui/movieplayer.cpp
@@ -1485,8 +1485,8 @@ bool CMoviePlayerGui::PlayFileStart(void)
 	getCurrentAudioName(is_file_player, currentaudioname);
 	if (is_file_player)
 		selectAutoLang();
-	if(!isLuaPlay)
-		enableOsdElements(MUTE);
+
+	enableOsdElements(MUTE);
 	return res;
 }
 

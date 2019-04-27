7fdf0d94174fc72539f659305b9143bc97d68e39
Jacek Jendrzej <overx300@gmail.com>
2019-04-28 00:32:34 +0200

    add option to adzap: zap on timer activation

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -1058,6 +1058,7 @@ int CNeutrinoApp::loadSetup(const char * fname)
 
 	g_settings.adzap_zapBackPeriod = configfile.getInt32("adzap_zapBackPeriod", 180);
 	g_settings.adzap_writeData = configfile.getInt32("adzap_writeData", 0);
+	g_settings.adzap_zapOnActivation = configfile.getInt32("adzap_zapOnActivation", 0);
 
 	// USERMENU -> in system/settings.h
 	//-------------------------------------------
@@ -1737,6 +1738,7 @@ void CNeutrinoApp::saveSetup(const char * fname)
 	configfile.setInt32("uselastchannel", g_settings.uselastchannel);
 	configfile.setInt32("adzap_zapBackPeriod", g_settings.adzap_zapBackPeriod);
 	configfile.setInt32("adzap_writeData", g_settings.adzap_writeData);
+	configfile.setInt32("adzap_zapOnActivation", g_settings.adzap_zapOnActivation);
 	//epg search
 	g_settings.epg_search_history_size = g_settings.epg_search_history.size();
 	if (g_settings.epg_search_history_size > g_settings.epg_search_history_max)

2f2a9f6f09de4663a1f143581361bc4879332bd6
Frankenstone <dampf_acc@yahoo.com>
2019-06-03 19:47:57 +0200

    fix after pick

--- src/gui/movieplayer.cpp
+++ src/gui/movieplayer.cpp
@@ -631,7 +631,6 @@ void CMoviePlayerGui::ClearFlags()
 	isLuaPlay = false;
 	isUPNP = false;
 	isWebChannel = false;
-	isYT = false;
 	is_file_player = false;
 	timeshift = TSHIFT_MODE_OFF;
 }
@@ -1756,7 +1755,7 @@ void CMoviePlayerGui::PlayFileLoop(void)
 				filelist_it = vzap_it;
 			}
 			fromInfoviewer = false;
-		} else if (timeshift == TSHIFT_MODE_OFF && !isWebChannel /* && !isYT */ && (msg == (neutrino_msg_t) g_settings.mpkey_next_repeat_mode)) {
+		} else if (timeshift == TSHIFT_MODE_OFF && !isWebChannel && (msg == (neutrino_msg_t) g_settings.mpkey_next_repeat_mode)) {
 			repeat_mode = (repeat_mode_enum)((int)repeat_mode + 1);
 			if (repeat_mode > (int) REPEAT_ALL)
 				repeat_mode = REPEAT_OFF;
@@ -2479,7 +2478,7 @@ void CMoviePlayerGui::handleMovieBrowser(neutrino_msg_t msg, int /*position*/)
 			if (p_movie_info->length == 0) {
 				p_movie_info->length = (float)duration / 60 / 1000 + 0.5;
 			}
-			if (!isYT && !isHTTP && !isUPNP)
+			if (!isHTTP && !isUPNP)
 				cMovieInfo.saveMovieInfo(*p_movie_info);
 			//p_movie_info->fileInfoStale(); //TODO: we might to tell the Moviebrowser that the movie info has changed, but this could cause long reload times  when reentering the Moviebrowser
 		}
@@ -2590,7 +2589,7 @@ void CMoviePlayerGui::handleMovieBrowser(neutrino_msg_t msg, int /*position*/)
 			new_bookmark.length = play_sec - new_bookmark.pos;
 			TRACE("[mp] commercial length: %d\r\n", new_bookmark.length);
 			if (cMovieInfo.addNewBookmark(p_movie_info, new_bookmark) == true) {
-				if (!isYT && !isHTTP && !isUPNP)
+				if (!isHTTP && !isUPNP)
 					cMovieInfo.saveMovieInfo(*p_movie_info);	/* save immediately in xml file */
 			}
 			new_bookmark.pos = 0;	// clear again, since this is used as flag for bookmark activity
@@ -2601,7 +2600,7 @@ void CMoviePlayerGui::handleMovieBrowser(neutrino_msg_t msg, int /*position*/)
 			new_bookmark.pos = play_sec;
 			TRACE("[mp] loop length: %d\r\n", new_bookmark.length);
 			if (cMovieInfo.addNewBookmark(p_movie_info, new_bookmark) == true) {
-				if (!isYT && !isHTTP && !isUPNP)
+				if (!isHTTP && !isUPNP)
 					cMovieInfo.saveMovieInfo(*p_movie_info);	/* save immediately in xml file */
 				jump_not_until = play_sec + 5;	// avoid jumping for this time
 			}
@@ -2671,7 +2670,7 @@ void CMoviePlayerGui::handleMovieBrowser(neutrino_msg_t msg, int /*position*/)
 				new_bookmark.pos = play_sec;
 				new_bookmark.length = 0;
 				if (cMovieInfo.addNewBookmark(p_movie_info, new_bookmark) == true)
-					if (!isYT && !isHTTP && !isUPNP)
+					if (!isHTTP && !isUPNP)
 						cMovieInfo.saveMovieInfo(*p_movie_info);	/* save immediately in xml file */
 				new_bookmark.pos = 0;	// clear again, since this is used as flag for bookmark activity
 			} else if (cSelectedMenuBookStart[3].selected == true) {
@@ -2688,13 +2687,13 @@ void CMoviePlayerGui::handleMovieBrowser(neutrino_msg_t msg, int /*position*/)
 				/* Moviebrowser movie start bookmark */
 				p_movie_info->bookmarks.start = play_sec;
 				TRACE("[mp] New movie start pos: %d\r\n", p_movie_info->bookmarks.start);
-				if (!isYT && !isHTTP && !isUPNP)
+				if (!isHTTP && !isUPNP)
 					cMovieInfo.saveMovieInfo(*p_movie_info);	/* save immediately in xml file */
 			} else if (cSelectedMenuBookStart[6].selected == true) {
 				/* Moviebrowser movie end bookmark */
 				p_movie_info->bookmarks.end = play_sec;
 				TRACE("[mp]  New movie end pos: %d\r\n", p_movie_info->bookmarks.end);
-				if (!isYT && !isHTTP && !isUPNP)
+				if (!isHTTP && !isUPNP)
 					cMovieInfo.saveMovieInfo(*p_movie_info);	/* save immediately in xml file */
 			}
 		}

212522eea908be53ac567520376b0b176ed57760
Frankenstone <dampf_acc@yahoo.com>
2019-06-10 12:22:27 +0200

    neutrino.cpp: lcd4l path

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -385,7 +385,7 @@ int CNeutrinoApp::loadSetup(const char * fname)
 
 #ifdef ENABLE_LCD4LINUX
 	g_settings.lcd4l_support = configfile.getInt32("lcd4l_support" , 0);
-	g_settings.lcd4l_logodir = configfile.getString("lcd4l_logodir", LOGODIR);
+	g_settings.lcd4l_logodir = configfile.getString("lcd4l_logodir", "/swap/logo-lcd");
 	g_settings.lcd4l_dpf_type = configfile.getInt32("lcd4l_dpf_type", 0);
 	g_settings.lcd4l_skin = configfile.getInt32("lcd4l_skin" , 0);
 	g_settings.lcd4l_skin_radio = configfile.getInt32("lcd4l_skin_radio" , 0);

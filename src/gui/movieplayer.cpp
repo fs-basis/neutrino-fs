2728a04f7ad2bd628900e2835843afd91fcd1532
Frankenstone <dampf_acc@yahoo.com>
2020-01-15 18:27:56 +0100

    fix remove upnp

--- src/gui/movieplayer.cpp
+++ src/gui/movieplayer.cpp
@@ -269,6 +269,9 @@ void CMoviePlayerGui::Init(void)
 void CMoviePlayerGui::cutNeutrino()
 {
 
+	if (playing)
+		return;
+
 #ifdef ENABLE_CHANGE_OSD_RESOLUTION
 	COsdHelpers *coh     = COsdHelpers::getInstance();
 	currentVideoSystem   = coh->getVideoSystem();
@@ -314,6 +317,10 @@ void CMoviePlayerGui::cutNeutrino()
 
 void CMoviePlayerGui::restoreNeutrino()
 {
+
+	if (!playing)
+		return;
+
 #ifdef ENABLE_CHANGE_OSD_RESOLUTION
 	if ((currentVideoSystem > -1) &&
 	    (g_settings.video_Mode == VIDEO_STD_AUTO) &&

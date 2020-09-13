ad20ac348de5ce7b072648444af16155eade4dab
Frankenstone <dampf_acc@online.de>
2020-09-13 08:27:35 +0200

    add webradiodir_var path

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -768,7 +768,7 @@ int CNeutrinoApp::loadSetup(const char * fname)
 #endif
 	g_settings.downloadcache_dir = configfile.getString( "downloadcache_dir", g_settings.network_nfs_recordingdir.c_str());
 	g_settings.last_webtv_dir = configfile.getString( "last_webtv_dir", WEBTVDIR_VAR);
-	g_settings.last_webradio_dir = configfile.getString( "last_webradio_dir", WEBTVDIR_VAR);
+	g_settings.last_webradio_dir = configfile.getString( "last_webradio_dir", WEBRADIODIR_VAR);
 
 	g_settings.timeshift_temp = configfile.getInt32( "timeshift_temp", 0 );
 	g_settings.timeshift_auto = configfile.getInt32( "timeshift_auto", 0 );

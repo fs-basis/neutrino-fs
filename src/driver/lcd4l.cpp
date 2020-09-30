02399a7ad3cd7be90bff8732a798e3bdadfe20df
vanhofen <vanhofen@gmx.de>
2020-09-30 21:07:36 +0200

    - lcd4l: show event in webtv/webradio mode

--- src/driver/lcd4l.cpp
+++ src/driver/lcd4l.cpp
@@ -48,6 +48,7 @@
 #include <driver/radiotext.h>
 #include <zapit/capmt.h>
 #include <zapit/zapit.h>
+#include <gui/infoviewer.h>
 #include <gui/movieplayer.h>
 #include <eitd/sectionsd.h>
 #include <hardware/video.h>
@@ -803,6 +804,12 @@ void CLCD4l::ParseInfo(uint64_t parseID, bool newID, bool firstRun)
 			Event += "\n" + CurrentNext.next_name;
 			tm_struct = localtime(&CurrentNext.next_zeit.startzeit);
 			snprintf(End, sizeof(End), "%02d:%02d", tm_struct->tm_hour, tm_struct->tm_min);
+		}
+
+		if (CNeutrinoApp::getInstance()->getMode() == NeutrinoModes::mode_webtv || CNeutrinoApp::getInstance()->getMode() == NeutrinoModes::mode_webradio)
+		{
+			// FIXME: Doesn't work with timing.infobar_tv/radio=0
+			Event = g_InfoViewer->get_livestreamInfo1();
 		}
 	}
 	else if (parseID == NeutrinoModes::mode_audio)

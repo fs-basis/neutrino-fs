820803768f88c6e6e08c8b46d99424abe88cbb04
Frankenstone <dampf_acc@online.de>
2020-10-03 10:47:30 +0200

    fix after pick

--- src/driver/display.h
+++ src/driver/display.h
@@ -2,13 +2,6 @@
 #if HAVE_DUCKBOX_HARDWARE
 #include <driver/vfd.h>
 #endif
-<<<<<<< HEAD
-#if HAVE_TRIPLEDRAGON
-#include <driver/lcdd.h>
-#endif
-#if HAVE_SPARK_HARDWARE || HAVE_AZBOX_HARDWARE || HAVE_GENERIC_HARDWARE  || HAVE_ARM_HARDWARE
-=======
-#if HAVE_SPARK_HARDWARE || HAVE_GENERIC_HARDWARE  || HAVE_ARM_HARDWARE || HAVE_MIPS_HARDWARE
->>>>>>> c531f49e3... coolstream hardware remove part 9
+#if HAVE_SPARK_HARDWARE || HAVE_GENERIC_HARDWARE  || HAVE_ARM_HARDWARE
 #include <driver/simple_display.h>
 #endif

645c9cb0e4b75efc30ffd8e811b7beb8d84b03e8
Frankenstone <dampf_acc@online.de>
2021-06-07 07:32:41 +0200

    libnet: small fix, thx BPanther

--- lib/libnet/libnet.cpp
+++ lib/libnet/libnet.cpp
@@ -13,7 +13,7 @@
 
 void	netGetIP(std::string &dev, std::string &ip, std::string &mask, std::string &brdcast)
 {
-	int					fd;
+	int			fd;
 	struct ifreq		req;
 	struct sockaddr_in	*saddr;
 	unsigned char		*addr;
@@ -25,7 +25,6 @@ void	netGetIP(std::string &dev, std::string &ip, std::string &mask, std::string 
 	fd=socket(AF_INET,SOCK_DGRAM,0);
 	if ( !fd )
 		return;
-
 
 	memset(&req,0,sizeof(req));
 	strncpy(req.ifr_name, dev.c_str(), sizeof(req.ifr_name)-1);
@@ -50,7 +49,7 @@ void	netGetIP(std::string &dev, std::string &ip, std::string &mask, std::string 
 	return;
 }
 
-void netGetDefaultRoute( std::string &ip )
+void	netGetDefaultRoute(std::string &ip)
 {
 	FILE *fp;
 	char interface[9];
@@ -80,7 +79,7 @@ void netGetDefaultRoute( std::string &ip )
 	fclose(fp);
 }
 
-void netGetHostname( std::string &host )
+void	netGetHostname(std::string &host)
 {
 	host = "";
 	char hostbuf[256];
@@ -88,7 +87,7 @@ void netGetHostname( std::string &host )
 		host = std::string(hostbuf);
 }
 
-void	netSetHostname( std::string &host )
+void	netSetHostname(std::string &host)
 {
 	if (sethostname(host.c_str(), host.length()) < 0)
 	{
@@ -125,7 +124,7 @@ void	netSetNameserver(std::string &ip)
 	fclose(fp);
 }
 
-void	netGetNameserver( std::string &ip )
+void	netGetNameserver(std::string &ip)
 {
 	FILE *fp;
 	char zeile[256];
@@ -156,7 +155,7 @@ void	netGetNameserver( std::string &ip )
 	fclose(fp);
 }
 
-void netGetMacAddr(std::string &ifname, unsigned char *mac)
+void	netGetMacAddr(std::string &ifname, unsigned char *mac)
 {
 	int fd;
 	struct ifreq ifr;

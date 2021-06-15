1891f521ed5b815c3fd5bc1515e9c11b29176aff
BPanther <bpanther_ts@hotmail.com>
2021-06-15 09:12:23 +0200

    clear configfile neutrino.conf

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -1234,6 +1234,9 @@ void CNeutrinoApp::saveSetup(const char * fname)
 		dprintf(DEBUG_NORMAL, "error while saving scan-settings!\n");
 	}
 
+	// clear configfile
+	configfile.clear();
+
 	//theme/color options
 	CThemes::setTheme(configfile);
 

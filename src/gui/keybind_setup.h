e048c057c46d9e2db273eec5fb2aaedf3a6035c8
Frankenstone <dampf_acc@online.de>
2021-12-05 10:44:06 +0100

    keybind: remove special keys, sondertasten

--- src/gui/keybind_setup.h
+++ src/gui/keybind_setup.h
@@ -88,9 +88,6 @@ class CKeybindSetup : public CMenuTarget, public CChangeObserver
 			NKEY_PIP_SETUP,
 			NKEY_PIP_SWAP,
 #endif
-			NKEY_FORMAT_MODE,
-			NKEY_PIC_MODE,
-			NKEY_PIC_SIZE,
 			NKEY_RECORD,
 			MBKEY_COPY_ONEFILE,
 			MBKEY_COPY_SEVERAL,
@@ -114,7 +111,6 @@ class CKeybindSetup : public CMenuTarget, public CChangeObserver
 		void showKeyBindQuickzapSetup(CMenuWidget *bindSettings_qzap);
 		void showKeyBindMovieplayerSetup(CMenuWidget *bindSettings_mplayer);
 		void showKeyBindMoviebrowserSetup(CMenuWidget *bindSettings_mbrowser);
-		void showKeyBindSpecialSetup(CMenuWidget *bindSettings_special);
 		int getRemoteCode();
 		bool setRemoteCode(int code);
 		

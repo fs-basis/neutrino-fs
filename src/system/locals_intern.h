4a5921637842c80b5c8fc763b85fc5a146174d14
Frankenstone <dampf_acc@online.de>
2021-12-07 18:40:03 +0100

    keybind_setup: remove KEYBINDINGMENU_BOUQUETHANDLING, gab es nie wirklich und wird auch nie ;-)

--- src/system/locals_intern.h
+++ src/system/locals_intern.h
@@ -879,7 +879,6 @@ const char * locale_real_names[] =
 	"keybindingmenu.addrecord",
 	"keybindingmenu.addremind",
 	"keybindingmenu.bouquetdown",
-	"keybindingmenu.bouquethandling",
 	"keybindingmenu.bouquetup",
 	"keybindingmenu.channeldown",
 	"keybindingmenu.channellist",

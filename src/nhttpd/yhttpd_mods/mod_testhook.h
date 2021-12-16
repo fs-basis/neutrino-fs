1c8e4ae2281c8649f92927f78e50241c05a7fcda
Frankenstone <dampf_acc@online.de>
2021-12-16 14:44:30 +0100

    src/nhpttd: code format using astyle

--- src/nhttpd/yhttpd_mods/mod_testhook.h
+++ src/nhttpd/yhttpd_mods/mod_testhook.h
@@ -9,12 +9,15 @@
 #include <yhook.h>
 class CTesthook : public Cyhook
 {
-public:
-	THandleStatus Hook_SendResponse(CyhookHandler *hh);
-	CTesthook(){};
-	~CTesthook(){};
+	public:
+		THandleStatus Hook_SendResponse(CyhookHandler *hh);
+		CTesthook() {};
+		~CTesthook() {};
 
-	virtual std::string getHookName(void) {return std::string("Testhook");}
+		virtual std::string getHookName(void)
+		{
+			return std::string("Testhook");
+		}
 
 };
 #endif /*TESTHOOK_H_*/

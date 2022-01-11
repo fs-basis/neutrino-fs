2a8f2ac733500d78d8050e785df06a0b8aaa2c2e
Frankenstone <dampf_acc@online.de>
2022-01-11 10:01:09 +0100

    locals: clean unused

--- src/system/locals_intern.h
+++ src/system/locals_intern.h
@@ -2422,7 +2422,6 @@ const char *locale_real_names[] =
 	"videomenu.psi.step",
 	"videomenu.psi.tint",
 	"videomenu.screensetup",
-	"videomenu.tv-scart",
 	"videomenu.videoformat",
 	"videomenu.videoformat_169",
 	"videomenu.videoformat_43",

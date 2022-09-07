046a3f6b9df8fc13eb4f6843689c73dddff002bb
Frankenstone <dampf_acc@online.de>
2022-09-07 07:41:05 +0200

    neutrino: fix commit 1d134374f

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -648,8 +648,8 @@ if (g_info.hw_caps->can_shutdown)
 	g_settings.timeshiftdir = configfile.getString( "timeshiftdir", "/media/sda1/timeshift" );
 #endif
 	g_settings.downloadcache_dir = configfile.getString( "downloadcache_dir", g_settings.network_nfs_recordingdir.c_str());
-	g_settings.last_webtv_dir = configfile.getString( "last_webtv_dir", CONFIGDIR);
-	g_settings.last_webradio_dir = configfile.getString( "last_webradio_dir", CONFIGDIR);
+	g_settings.last_webtv_dir = configfile.getString( "last_webtv_dir", WEBCHANNELS);
+	g_settings.last_webradio_dir = configfile.getString( "last_webradio_dir", WEBCHANNELS);
 
 	g_settings.timeshift_temp = configfile.getInt32( "timeshift_temp", 0 );
 	g_settings.timeshift_auto = configfile.getInt32( "timeshift_auto", 0 );

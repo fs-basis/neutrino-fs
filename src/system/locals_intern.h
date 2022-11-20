7825d9eeddcc814ac9b4fb67eb516ad1e849083c
Frankenstone <dampf_acc@online.de>
2022-11-20 07:21:37 +0100

    videosettings: readd zappimgmode

--- src/system/locals_intern.h
+++ src/system/locals_intern.h
@@ -2400,6 +2400,8 @@ const char *locale_real_names[] =
 	"videomenu.zappingmode",
 	"videomenu.zappingmode_mute",
 	"videomenu.zappingmode_hold",
+	"videomenu.zappingmode_mutetilllock",
+	"videomenu.zappingmode_holdtilllock",
 	"webradio.head",
 	"webradio.xml",
 	"webradio.xml.add",

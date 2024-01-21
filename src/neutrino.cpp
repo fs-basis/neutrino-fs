813c87090feab8cd3ca89be84e47ee7e4e1234b2
Frankenstone <dampf_acc@online.de>
2024-01-21 20:12:57 +0100

    neutrino: fix pip, thx BPanther

--- src/neutrino.cpp
+++ src/neutrino.cpp
@@ -2508,8 +2508,8 @@ TIMER_STOP("################################## after all #######################
 
 	xmltv_xml_readepg();
 	xmltv_xml_auto_readepg();
-#if 0 // Box nicht definiert
-#if ENABLE_PIP && BOXMODEL_E4HDULTRA
+
+#if ENABLE_PIP
 	if (g_info.hw_caps->can_pip)
 	{
 		CZapit::getInstance()->OpenPip(0);
@@ -2518,7 +2518,7 @@ TIMER_STOP("################################## after all #######################
 		CZapit::getInstance()->StopPip(0);
 	}
 #endif
-#endif //if 0
+
 	RealRun();
 	ExitRun(CNeutrinoApp::EXIT_REBOOT);
 
